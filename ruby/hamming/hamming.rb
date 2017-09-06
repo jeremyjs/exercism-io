7af9c8624df9d022d39d2ff9885c78d59149a85b
Jeremy Meyer <jeremym@squareup.com>
2017-09-05 23:18:21 -0700

    ruby/hamming: convert to module_function

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,16 +1,18 @@
-class Hamming
-  def self.compute(strand_1, strand_2)
+module Hamming
+  module_function
+
+  def compute(strand_1, strand_2)
     if strand_1.length > strand_2.length
       strand_1, strand_2 = strand_2, strand_1
     end
     difference(strand_1, strand_2)
   end
 
-  def self.difference(strand_1, strand_2)
+  def difference(strand_1, strand_2)
     strand_1.length.times.count { |i| difference?(strand_1[i], strand_2[i]) }
   end
 
-  def self.difference?(strand_1, strand_2)
+  def difference?(strand_1, strand_2)
     if strand_1.length <= 1
       strand_1 != strand_2[0]
     else
@@ -18,4 +20,3 @@ class Hamming
     end
   end
 end
-
