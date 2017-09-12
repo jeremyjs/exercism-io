b9008c4ebd210c561bc9ae036da34aa9696b664c
Jeremy Meyer <jeremym@squareup.com>
2017-09-11 20:41:33 -0700

    ruby/hamming tersify!

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,27 +1,9 @@
-module Hamming
-  module_function
-
-  def compute(strand_1, strand_2)
-    raise ArgumentError unless strand_1.length.eql? strand_2.length
-    hamming_distance(strand_1, strand_2)
-  end
-
-  private_class_method
-
-  def chars_differ(char_1, char_2)
-    char_1 != char_2
-  end
-
-  def hamming_distance(strand_1, strand_2)
-    num_different_chars(zipped_strands(strand_1, strand_2))
-  end
-
-  def num_different_chars(zipped_strands)
-    zipped_strands.count { |pair| chars_differ(*pair) }
-  end
-
-  def zipped_strands(strand_1, strand_2)
-    strand_1.chars.zip(strand_2.chars)
+class Hamming
+  def self.compute(strand_1, strand_2)
+    raise ArgumentError if strand_1.length != strand_2.length
+    strand_1.chars.zip(strand_2.chars).count do |char_1, char_2|
+      char_1 != char_2
+    end
   end
 end
 
