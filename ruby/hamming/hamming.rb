77603e6c8ddc851742db21f7d77df08dfeea5bf4
Jeremy Meyer <jeremym@squareup.com>
2017-09-11 19:55:41 -0700

    ruby/hamming refactor for great good

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,18 +1,27 @@
-
 module Hamming
   module_function
 
   def compute(strand_1, strand_2)
     raise ArgumentError unless strand_1.length.eql? strand_2.length
-    zipped_strands(strand_1, strand_2).map { |pair| chars_eql(pair) }.reject { |result| !!result }.count
+    hamming_distance(strand_1, strand_2)
+  end
+
+  private_class_method
+
+  def chars_differ(char_1, char_2)
+    char_1 != char_2
+  end
+
+  def hamming_distance(strand_1, strand_2)
+    num_different_chars(zipped_strands(strand_1, strand_2))
+  end
+
+  def num_different_chars(zipped_strands)
+    zipped_strands.count { |pair| chars_differ(*pair) }
   end
 
   def zipped_strands(strand_1, strand_2)
     strand_1.chars.zip(strand_2.chars)
-  end
-
-  def chars_eql(chars)
-    chars[0].eql? chars[1]
   end
 end
 
